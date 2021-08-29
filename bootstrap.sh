78e7cd2a55dde75a3ec945e9e9eec879a380c3a9
Luboš Pokorný <lubos.pokorny@gmail.com>
2021-08-29 16:49:52 +0200

    better email prompt

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,8 +13,12 @@ sh -c "$(curl -fsLS git.io/chezmoi)" -- -b "${BIN_DIR}"
 curl "${OP_URL}" --f --output "${OP}"
 chmod +x "${OP}"
 
-echo -n "Enter 1password account e-mail"
+set +x
+echo
+echo -n "Enter 1password account e-mail: "
 read -r OP_EMAIL
+echo
+set -x
 
 "${OP}" signin my "${OP_EMAIL}"
 eval "$(${OP} signin my)"
