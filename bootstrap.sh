fbdd574c96bc1100974320fa2ca9f5cd1c9ef9f4
Luboš Pokorný <lubos.pokorny@gmail.com>
2021-08-29 19:01:23 +0200

    sppedup interrupted boostrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,29 +5,39 @@ set -euo pipefail
 OP_URL="https://raw.githubusercontent.com/zoido/dotfiles/main/dot_local/bin/executable_op"
 BIN_DIR="${HOME}/.local/bin"
 OP="${BIN_DIR}/op"
+CHEZMOI="${BIN_DIR}/chezmoi"
 
 mkdir -p "${BIN_DIR}"
 
-sh -c "$(curl -fsLS git.io/chezmoi)" -- -b "${BIN_DIR}"
+if [ ! -f "${CHEZMOI}" ]; then
+    sh -c "$(curl -fsLS git.io/chezmoi)" -- -b "${BIN_DIR}"
+fi
 
-curl "${OP_URL}" -f --output "${OP}"
-chmod +x "${OP}"
+if [ ! -f "${OP}" ]; then
+    curl "${OP_URL}" -f --output "${OP}"
+    chmod +x "${OP}"
+fi
 
-echo
-echo -n "Enter 1password account e-mail: "
-read -r OP_EMAIL
-echo
+if [ ! -f "${HOME}/.config/op/config" ]; then
+    echo
+    echo -n "Enter 1password account e-mail: "
+    read -r OP_EMAIL
+    echo
 
-while ! "${OP}" signin my "${OP_EMAIL}"; do
-    echo "Try again."
-done
+    while ! "${OP}" signin my "${OP_EMAIL}"; do
+        echo "Try again."
+    done
+fi
 
-set +e
-while true; do
-    op="$(${OP} signin my)"
-    [ "$?" -ne 0 ] && echo "Try again." && continue
-    eval "${op}"
-    break
-done
 
-"${BIN_DIR}/chezmoi" init --apply zoido
+if ! "${OP}" get account; then
+    set +e
+    while true; do
+        op="$(${OP} signin)"
+        [ "$?" -ne 0 ] && echo "Try again." && continue
+        eval "${op}"
+        break
+    done
+fi
+
+"${CHEZMOI}" init --apply zoido
