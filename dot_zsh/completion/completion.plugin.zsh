6ca52914d445b494321dc9fe83b164157f2ed798
Luboš Pokorný <lubos.pokorny@gmail.com>
2021-09-04 08:57:21 +0200

    don;t fail on pew completion

--- dot_zsh/completion/completion.plugin.zsh
+++ dot_zsh/completion/completion.plugin.zsh
@@ -1,5 +1,3 @@
-# fpath[1,0]=/usr/share/zsh/site-functions
-
-[ "$(command -v chezmoi)" ] &&  eval "$(chezmoi completion zsh)"
-[ "$(command -v op)" ] &&  eval "$(op completion zsh)"
-[ "$(command -v pew)" ] &&  source $(pew shell_config)
+[ "$(command -v chezmoi)" ] && eval "$(chezmoi completion zsh)"
+[ "$(command -v op)" ] && eval "$(op completion zsh)"
+[ "$(command -v pew)" ] && source $(pew shell_config) || exit 0
