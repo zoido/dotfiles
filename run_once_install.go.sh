5a41a60162a615dfc1bfbd0591bfab4366c2cc9b
Luboš Pokorný <lubos.pokorny@gmail.com>
2021-11-11 12:16:28 +0100

    go: grpcui

--- run_once_install.go.sh
+++ run_once_install.go.sh
@@ -3,6 +3,7 @@
 packages=(
     github.com/ktr0731/evans@latest
     github.com/bufbuild/buf/cmd/buf@latest
+    github.com/fullstorydev/grpcui/cmd/grpcui@latest
 )
 
 for package in ${packages[*]}; do
