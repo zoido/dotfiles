0382be62fa4e4e503659341662970dea96f49cd4
Luboš Pokorný <lubos.pokorny@gmail.com>
2021-12-21 07:57:08 +0100

    go add kind

--- run_once_install.go.sh
+++ run_once_install.go.sh
@@ -4,6 +4,7 @@ packages=(
     github.com/ktr0731/evans@latest
     github.com/bufbuild/buf/cmd/buf@v1.0.0-rc6
     github.com/fullstorydev/grpcui/cmd/grpcui@latest
+    sigs.k8s.io/kind@v0.11.1
 )
 
 for package in ${packages[*]}; do
