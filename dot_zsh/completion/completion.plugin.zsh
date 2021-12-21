67e5d7ab439980740c4ddf56650b6efb70340a40
Luboš Pokorný <lubos.pokorny@gmail.com>
2021-12-21 08:14:48 +0100

    add kind

--- dot_zsh/completion/completion.plugin.zsh
+++ dot_zsh/completion/completion.plugin.zsh
@@ -4,3 +4,4 @@
 [ "$(command -v kubectl)" ] && source <(kubectl completion zsh)
 [ "$(command -v op)" ] && eval "$(op completion zsh)"
 [ "$(command -v pew)" ] && source $(pew shell_config) || true
+[ "$(command -v kind)" ] && source <(kind completion zsh)
