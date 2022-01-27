1c2b545eddd5530db0e99d9f7799a99a8ebbde70
Luboš Pokorný <zoido@users.noreply.github.com>
2022-01-27 09:37:40 +0100

    ping buf version.

--- run_onchange_install.go.sh
+++ run_onchange_install.go.sh
@@ -2,7 +2,7 @@
 
 packages=(
     github.com/ktr0731/evans@latest
-    github.com/bufbuild/buf/cmd/buf@latest
+    github.com/bufbuild/buf/cmd/buf@v1.0.0-rc11
     github.com/fullstorydev/grpcui/cmd/grpcui@latest
     sigs.k8s.io/kind@v0.11.1
 )
