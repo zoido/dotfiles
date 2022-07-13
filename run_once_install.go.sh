fc75aefe5ebd36ab56af5173090c8f0a48878ddf
Luboš Pokorný <zoido@users.noreply.github.com>
2022-07-13 09:11:38 +0200

    add grpc debug

--- run_once_install.go.sh
+++ run_once_install.go.sh
@@ -3,6 +3,7 @@
 packages=(
     github.com/bufbuild/buf/cmd/buf@latest
     github.com/fullstorydev/grpcui/cmd/grpcui@latest
+    github.com/grpc-ecosystem/grpcdebug@latest
     github.com/ktr0731/evans@latest
     github.com/mitranim/gow@latest
     github.com/terraform-docs/terraform-docs@latest
