0f86f642d93fa8507c750370659f9af22b4b6f3f
Luboš Pokorný <zoido@users.noreply.github.com>
2022-07-19 15:00:51 +0200

    go add tproxy

--- run_once_install.go.sh
+++ run_once_install.go.sh
@@ -4,6 +4,7 @@ packages=(
     github.com/bufbuild/buf/cmd/buf@latest
     github.com/fullstorydev/grpcui/cmd/grpcui@latest
     github.com/grpc-ecosystem/grpcdebug@latest
+    github.com/kevwan/tproxy@latest
     github.com/ktr0731/evans@latest
     github.com/mitranim/gow@latest
     github.com/terraform-docs/terraform-docs@latest
