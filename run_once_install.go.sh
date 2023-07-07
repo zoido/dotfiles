7d043d29054b971b9cd2fc8e6be100b5a9416222
Luboš Pokorný <zoido@users.noreply.github.com>
2023-07-07 10:24:03 +0200

    add tailer to go install

--- run_once_install.go.sh
+++ run_once_install.go.sh
@@ -4,12 +4,13 @@ packages=(
     github.com/bufbuild/buf/cmd/buf@latest
     github.com/fullstorydev/grpcui/cmd/grpcui@latest
     github.com/grpc-ecosystem/grpcdebug@latest
+    github.com/hionay/tailer/cmd/tailer@latest
     github.com/kevwan/tproxy@latest
     github.com/ktr0731/evans@latest
     github.com/noborus/ov@latest
     github.com/terraform-docs/terraform-docs@latest
-    sigs.k8s.io/kind@latest
     mvdan.cc/gofumpt@latest
+    sigs.k8s.io/kind@latest
 )
 
 for package in "${packages[@]}"; do
