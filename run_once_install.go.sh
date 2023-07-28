28b043c8751818835c1e97ea54bd5e69a2c39094
Luboš Pokorný <zoido@users.noreply.github.com>
2023-07-28 09:01:54 +0200

    add vcluster

--- run_once_install.go.sh
+++ run_once_install.go.sh
@@ -7,6 +7,8 @@ packages=(
     github.com/hionay/tailer/cmd/tailer@latest
     github.com/kevwan/tproxy@latest
     github.com/ktr0731/evans@latest
+    github.com/loft-sh/vcluster/cmd/vcluster@latest
+    github.com/loft-sh/vcluster/cmd/vclusterctl@latest
     github.com/noborus/ov@latest
     github.com/terraform-docs/terraform-docs@latest
     mvdan.cc/gofumpt@latest
