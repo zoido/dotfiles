a9688b04d7e7c4dfd43f09e06d074de8d46f1022
Luboš Pokorný <zoido@users.noreply.github.com>
2023-08-12 15:22:06 +0200

    Update run_once_install.tpm.sh

--- run_once_install.tpm.sh
+++ run_once_install.tpm.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+=#!/bin/bash
 
 HOME_=${HOME}
-git clone https://github.com/tmux-plugins/tpm ${HOME_}/.tmux/plugins/tpm
+HOME= git clone https://github.com/tmux-plugins/tpm ${HOME_}/.tmux/plugins/tpm
 ${HOME_}/.tmux/plugins/tpm/bin/install_plugins
