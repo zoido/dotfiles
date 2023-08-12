75866cc0bda32942c9c699221a3b30f6b52703d4
Luboš Pokorný <zoido@users.noreply.github.com>
2023-08-12 14:11:29 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,23 +4,15 @@ set -eo pipefail
 
 OP_URL="https://raw.githubusercontent.com/zoido/dotfiles/main/dot_local/bin/executable_op"
 BIN_DIR="${HOME}/.local/bin"
-OP="${BIN_DIR}/op"
 CHEZMOI="${BIN_DIR}/chezmoi"
 
 install_chezmoi() {
     sh -c "$(curl -fsLS chezmoi.io/get)" -- -b "${BIN_DIR}"
 }
 
-download_op() {
-    curl "${OP_URL}" -f --output "${OP}"
-    chmod +x "${OP}"
-}
-
-
 mkdir -p "${BIN_DIR}"
 
 [ ! -f "${CHEZMOI}" ] && install_chezmoi
-[ ! -f "${OP}" ] && download_op
 
 if [ "${CODESPACES}" == "true" ]; then
     SCRIPT_DIR="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
