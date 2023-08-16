b92b462932d9889b2738570ec2c3a8a04d444035
Luboš Pokorný <zoido@users.noreply.github.com>
2023-08-16 08:59:08 +0200

    undo explicit git ignoring and go with selective git config

--- run_once_010-install.zgenom.sh
+++ run_once_010-install.zgenom.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-GIT_CONFIG_GLOBAL='' git clone https://github.com/jandamm/zgenom.git "${HOME}/.zgenom"
+git clone https://github.com/jandamm/zgenom.git "${HOME}/.zgenom"
 exit 0
--- run_once_install.tpm.sh
+++ run_once_install.tpm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-GIT_CONFIG_GLOBAL='' git clone https://github.com/tmux-plugins/tpm ${HOME}/.tmux/plugins/tpm
-GIT_CONFIG_GLOBAL='' ${HOME}/.tmux/plugins/tpm/bin/install_plugins
+git clone https://github.com/tmux-plugins/tpm ${HOME}/.tmux/plugins/tpm
+${HOME}/.tmux/plugins/tpm/bin/install_plugins
