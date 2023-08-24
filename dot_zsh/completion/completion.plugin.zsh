76b518d889a2e6c9fec9c865bebf7e58eef0db08
Luboš Pokorný <zoido@users.noreply.github.com>
2023-08-24 07:41:19 +0200

    add completions for pipx

--- dot_zsh/completion/completion.plugin.zsh
+++ dot_zsh/completion/completion.plugin.zsh
@@ -1,7 +1,8 @@
 [ "$(command -v argo)" ] && source <(argo completion zsh) || true
 [ "$(command -v chezmoi)" ] && eval "$(chezmoi completion zsh)"
 [ "$(command -v helm)" ] && source <(helm completion zsh)
+[ "$(command -v kind)" ] && source <(kind completion zsh)
 [ "$(command -v kubectl)" ] && source <(kubectl completion zsh)
 [ "$(command -v op)" ] && eval "$(op completion zsh)"
 [ "$(command -v pew)" ] && source $(pew shell_config) || true
-[ "$(command -v kind)" ] && source <(kind completion zsh)
+[ "$(command -v pipx)" ] && eval "$(register-python-argcomplete pipx)"
