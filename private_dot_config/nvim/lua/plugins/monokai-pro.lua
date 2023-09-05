87d9072d9b349bf142810aa6598393f40732f46e
Luboš Pokorný <zoido@users.noreply.github.com>
2023-09-05 13:25:37 +0200

    experiment with neovim

---
+++ private_dot_config/nvim/lua/plugins/monokai-pro.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "loctvl842/monokai-pro.nvim",
+    opts = {
+      transparent_background = false,
+      italics = true,
+      filter = "octagon",
+      background_clear = {},
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "monokai-pro",
+    },
+  },
+}
