6f891249e0eb89b49db0eb9030ce52a79cba3c7e
Luboš Pokorný <zoido@users.noreply.github.com>
2023-10-02 10:28:58 +0200

    nvim: add neogit

---
+++ private_dot_config/nvim_config/lua/plugins/neogit.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "nvim-telescope/telescope.nvim", -- optional
+      "sindrets/diffview.nvim",        -- optional
+      "ibhagwan/fzf-lua",              -- optional
+    },
+    opts = {
+      kind = "auto"
+    },
+    config = true,
+  },
+}
