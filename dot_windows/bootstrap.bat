43d5a32f242712353dce9a0b57971cfe90d7ce0c
Luboš Pokorný <zoido@users.noreply.github.com>
2024-02-15 21:04:21 +0100

    .exe to chezmpoi

--- dot_windows/bootstrap.bat
+++ dot_windows/bootstrap.bat
@@ -1,4 +1,4 @@
 winget install --accept-package-agreements --accept-source-agreements twpayne.chezmoi
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-chezmoi init --apply zoido
-chezmoi update
+chezmoi.exe init --apply zoido
+chezmoi.exe update
