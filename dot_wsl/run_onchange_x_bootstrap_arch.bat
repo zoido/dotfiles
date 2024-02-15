dee538cb498ff703fc75b715a4907a4b3e0bdb11
Luboš Pokorný <zoido@users.noreply.github.com>
2024-02-15 21:26:19 +0100

    relative path ok

--- dot_wsl/run_onchange_x_bootstrap_arch.bat
+++ dot_wsl/run_onchange_x_bootstrap_arch.bat
@@ -1,3 +1,3 @@
-Arch/ArchX.exe
-Arch/ArchX.exe runp bootstrap_arch.sh"
-Arch/ArchX.exe config --default-user zoid
+./Arch/ArchX.exe
+./Arch/ArchX.exe runp ./bootstrap_arch.sh
+./Arch/ArchX.exe config --default-user zoid
