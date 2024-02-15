3618328a7472feda43b62c0a9f81af95eb5c1183
Luboš Pokorný <zoido@users.noreply.github.com>
2024-02-15 21:31:54 +0100

    rmove ps1 varinat.

--- dot_wsl/bootstrap_arch.ps1
+++
@@ -1,3 +0,0 @@
-& "$env:USERPROFILE/.wsl/Arch/Arch.exe"
-& "$env:USERPROFILE/.wsl/Arch/Arch.exe" runp "$env:USERPROFILE/.wsl\bootstrap_arch.sh"
-& "$env:USERPROFILE/.wsl/Arch/Arch.exe" config --default-user zoid
