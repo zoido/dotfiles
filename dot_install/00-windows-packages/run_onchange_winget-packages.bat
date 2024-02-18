81355f2570105a8e68230ca8f68107b6c3bda672
Luboš Pokorný <zoido@users.noreply.github.com>
2024-02-18 14:44:15 +0100

    Start playing with Espanso

--- dot_install/00-windows-packages/run_onchange_winget-packages.bat
+++ dot_install/00-windows-packages/run_onchange_winget-packages.bat
@@ -1,3 +1,4 @@
+winget install -e --source winget --id "Espanso.Espanso"
 winget install -e --source winget --id "Git.Git"
-winget install -e --source winget --id "Golang.Go"
+winget install -e --source winget --id "GoLang.Go"
 winget install -e --source winget --id "Microsoft.WindowsTerminal"
