1c7af33a48ab973d1641405e251883538cf8df89
Luboš Pokorný <zoido@users.noreply.github.com>
2025-04-14 19:51:02 +0200

    install firefox with winget.

--- dot_install/00-windows-packages/run_onchange_winget-packages.bat
+++ dot_install/00-windows-packages/run_onchange_winget-packages.bat
@@ -4,6 +4,10 @@ winget install -e --source winget --id "Git.Git"
 winget install -e --source winget --id "GoLang.Go"
 winget install -e --source winget --id "M2Team.NanaZip"
 winget install -e --source winget --id "Microsoft.WindowsTerminal"
+winget install -e --source winget --id "Mozilla.Firefox"
 winget install -e --source winget --id "Qalculate.Qalculate"
 winget install -e --source winget --id "SumatraPDF.SumatraPDF"
 winget install -e --source winget --id "Wilfred.difftastic"
+
+rem Don't exit with error when there's not update for the last package.
+exit 0
