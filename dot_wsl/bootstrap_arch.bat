5e76aa8b2ffbf52d93a1b3aaeebf63e5a274a42d
Luboš Pokorný <zoido@users.noreply.github.com>
2025-04-25 09:31:37 +0200

    Cleaner more minimal WSL2 setup.

--- dot_wsl/bootstrap_arch.bat
+++ dot_wsl/bootstrap_arch.bat
@@ -1,4 +1,5 @@
-%USERPROFILE%/.wsl/Arch/Arch.exe
-%USERPROFILE%/.wsl/Arch/Arch.exe run sleep 5
-%USERPROFILE%/.wsl/Arch/Arch.exe runp %USERPROFILE%/.wsl/bootstrap_arch.sh
-%USERPROFILE%/.wsl/Arch/Arch.exe config --default-user zoid
+wsl --update
+wsl --install archlinux --name Arch
+wsl -d Arch --cd %USERPROFILE%/.wsl -- sh -c "tr -d '\r' < bootstrap_arch.sh | bash"
+wsl --terminate Arch
+wsl -d Arch -- sh -c "$(curl -fsLS https://raw.githubusercontent.com/zoido/dotfiles/main/bootstrap.sh)"
--- dot_wsl/bootstrap_arch.sh
+++ dot_wsl/bootstrap_arch.sh
@@ -2,48 +2,28 @@
 
 set -eo pipefail
 
-
-echo "Set root password..."
-until passwd; do
-  echo "Command failed, retrying..."
-  sleep 1
-done
-
+echo "Setting up pacman..."
+pacman -Syyu --noconfirm
+pacman -S sudo pwgen zsh --noconfirm
 
 echo "Setting up sudoers..."
+pwgen  16 1 | passwd  --stdin
+pacman -S sudo pwgen --noconfirm
 echo "%wheel ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/wheel
-
-echo "Arbitrary Pause..."
-# This helps with the keyring setup below to succeed for some reason.
-sleep 5
-
-echo "Setting up pacman..."
-pacman-key --init
-pacman-key --populate
-pacman -Syy --needed --noconfirm archlinux-keyring
-
-
-if grep -qF "[wslutilities]" /etc/pacman.conf; then
-  echo "wslutilities already in pacman.conf"
-else
-echo "Adding wslutilities to pacman.conf..."
-cat <<-"EOF" >> /etc/pacman.conf
-[wslutilities]
-Server = https://pkg.wslutiliti.es/arch/
-EOF
-fi
-
-curl https://pkg.wslutiliti.es/public.key | sudo pacman-key --add /dev/stdin
-pacman-key --lsign-key 2D4C887EB08424F157151C493DD50AA7E055D853
-
-pacman -Syyu --noconfirm
-pacman -S --noconfirm zsh
-
 
 echo "Setting up default user..."
 useradd -m -G wheel -s /usr/bin/zsh zoid
-echo "Set user password..."
-until passwd zoid; do
-  echo "Command failed, retrying..."
-  sleep 1
-done
+pwgen  16 1 | passwd zoid --stdin
+
+
+echo "Configuring WSL..."
+cat <<-"EOF" > /etc/wsl.conf
+[interop]
+enabled = true
+
+[boot]
+systemd=true
+
+[user]
+default = zoid
+EOF
