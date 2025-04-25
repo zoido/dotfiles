aaf7ff7624a4b82399f2442b8c9ef600269c3a51
Luboš Pokorný <zoido@users.noreply.github.com>
2025-04-25 12:45:10 +0200

    fix in arch bootstrap path

--- dot_wsl/bootstrap_arch.bat
+++ dot_wsl/bootstrap_arch.bat
@@ -1,5 +1,5 @@
 wsl --update
 wsl --install archlinux --name Arch
-wsl -d Arch --cd %USERPROFILE%/.wsl -- sh -c "tr -d '\r' < bootstrap_arch.sh | bash"
+wsl -d Arch --cd %USERPROFILE%\.wsl -- sh -c "tr -d '\r' < bootstrap_arch.sh | bash"
 wsl --terminate Arch
 wsl -d Arch -- sh -c "$(curl -fsLS https://raw.githubusercontent.com/zoido/dotfiles/main/bootstrap.sh)"
