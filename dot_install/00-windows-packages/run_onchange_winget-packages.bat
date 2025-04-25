8cbe60ecaa4e2a2a4a992ffadc49eb5cc6b56fa5
Luboš Pokorný <zoido@users.noreply.github.com>
2025-04-25 10:26:40 +0200

    install  vscode via winget

--- dot_install/00-windows-packages/run_onchange_winget-packages.bat
+++ dot_install/00-windows-packages/run_onchange_winget-packages.bat
@@ -4,12 +4,14 @@ winget install -e --source winget --id "Espanso.Espanso"
 winget install -e --source winget --id "Git.Git"
 winget install -e --source winget --id "GoLang.Go"
 winget install -e --source winget --id "M2Team.NanaZip"
+winget install -e --source winget --id "Microsoft.VisualStudioCode.CLI"
+winget install -e --source winget --id "Microsoft.VisualStudioCode"
 winget install -e --source winget --id "Microsoft.WindowsTerminal"
 winget install -e --source winget --id "Mozilla.Firefox"
-winget install -e --source winget --id "Python.Python.3.13"
 winget install -e --source winget --id "Qalculate.Qalculate"
 winget install -e --source winget --id "SumatraPDF.SumatraPDF"
 winget install -e --source winget --id "Wilfred.difftastic"
+winget install -e --source winget --moniker "python3"
 
 @REM Don't exit with error when there's not update for the last package.
 exit 0
