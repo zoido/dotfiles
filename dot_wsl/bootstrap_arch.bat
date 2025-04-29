d92ddb9052ebf491d10b3ed907c8769393f557d1
Luboš Pokorný <zoido@users.noreply.github.com>
2025-04-29 15:47:39 +0200

    Allow to set name of th boostrap Arch

--- dot_wsl/bootstrap_arch.bat
+++ dot_wsl/bootstrap_arch.bat
@@ -1,5 +1,6 @@
+IF NOT DEFINED ARCH_NAME SET "ARCH_NAME=Arch"
 wsl --update
-wsl --install archlinux --name Arch
+wsl --install archlinux --name %ARCH_NAME%
 wsl -d Arch --cd %USERPROFILE%\.wsl -- sh -c "tr -d '\r' < bootstrap_arch.sh | bash"
-wsl --terminate Arch
+wsl --terminate %ARCH_NAME%
 wsl -d Arch -- sh -c "$(curl -fsLS https://raw.githubusercontent.com/zoido/dotfiles/main/bootstrap.sh)"
