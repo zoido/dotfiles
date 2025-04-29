10ff4800f6ede7b42bddae9640c9cf17a12de887
Luboš Pokorný <zoido@users.noreply.github.com>
2025-04-29 14:47:47 +0200

    use `wsl-open`

--- dot_install/00-linux-packages/run_onchange_arch-wsl-packages.sh
+++ dot_install/00-linux-packages/run_onchange_arch-wsl-packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-yay -S --needed --noconfirm --sudoloop wsl2-ssh-agent
+yay -S --needed --noconfirm --sudoloop wsl2-ssh-agent wsl-open
 sudo libtool --finish /usr/lib/libfakeroot
