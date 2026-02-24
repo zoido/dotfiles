90eb05ca6632255bf458d096c95f94417fb51bd6
Luboš Pokorný <zoido@users.noreply.github.com>
2026-02-24 10:15:38 +0100

    Revert "fix wsl agent in fis"
    This reverts commit 86ad46c2e4d7aa99b75bcaab89d29a88a349881f.

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -22,6 +22,5 @@ if status is-interactive
 end
 
 if command -q "/usr/sbin/wsl2-ssh-agent"
-    /usr/sbin/wsl2-ssh-agent &> /dev/null
-    set SH_AUTH_SOCK "$HOME/.ssh/wsl2-ssh-agent.sock"
+    /usr/sbin/wsl2-ssh-agent | source
 end
