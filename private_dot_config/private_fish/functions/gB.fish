523ae5dc2095904c0cec26383b5bf92f2b4f4658
Luboš Pokorný <zoido@users.noreply.github.com>
2026-05-11 21:05:34 +0200

    improve git fzf helpers

--- private_dot_config/private_fish/functions/gB.fish
+++ private_dot_config/private_fish/functions/gB.fish
@@ -1,3 +1,7 @@
 function gB --description 'Switch git branch with fzf'
-    git branch | fzf | xargs git switch
+    git branch --sort=-committerdate |
+    grep -v '^\*' |
+    fzf --preview='git log --stat --decorate --color=always -1 {1}' |
+    awk '{print $NF}' |
+    xargs --no-run-if-empty git switch
 end
--- private_dot_config/private_fish/functions/gBD.fish
+++ private_dot_config/private_fish/functions/gBD.fish
@@ -1,3 +1,7 @@
 function gBD --description 'Delete git branches with fzf multi-select'
-    git branch | fzf -m | xargs git branch -D
+    git branch --sort=-committerdate |
+    grep -v '^\*' |
+    fzf -m --preview='git log --stat --decorate --color=always -1 {1}' |
+    awk '{print $NF}' |
+    xargs --no-run-if-empty git branch -D
 end
