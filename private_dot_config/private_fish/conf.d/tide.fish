cb901e1dd05fe004f86facd4c49b94e7a93e8cf7
Luboš Pokorný <zoido@users.noreply.github.com>
2026-09-09 16:56:41 +0200

    Improve fish startup time

--- private_dot_config/private_fish/conf.d/tide.fish
+++ private_dot_config/private_fish/conf.d/tide.fish
@@ -1,7 +1,7 @@
 # General prompt
 
 set -g tide_left_prompt_items pwd git newline time character
-set -g tide_right_prompt_items status cmd_duration context jobs direnv python rustc java php pulumi ruby go gcloud kubectl distrobox toolbox terraform aws nix_shell crystal elixir zig
+set -g tide_right_prompt_items status cmd_duration context jobs direnv python rustc go kubectl terraform aws
 # set -g tide_left_prompt_items pwd git newline character
 # set -g tide_right_prompt_items status cmd_duration context jobs direnv bun node python rustc java php pulumi ruby go gcloud kubectl distrobox toolbox terraform aws nix_shell crystal elixir zig time
 
